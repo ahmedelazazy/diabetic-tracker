944e6784eba66396a93403d7548f668c6244282b
Ahmed Elazazy <ahmed.elazazy@hotmail.com>
2023-08-30 18:31:03 +0200

    Adding service worker

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,14 +2,20 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    title: `diabetic-tracker`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-postcss", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }]
-};
+	siteMetadata: {
+		title: `diabetic-tracker`,
+		siteUrl: `https://www.yourdomain.tld`,
+	},
+	plugins: [
+		'gatsby-plugin-postcss',
+		{
+			resolve: 'gatsby-plugin-manifest',
+			options: {
+				icon: 'src/images/icon.png',
+			},
+		},
+
+		'gatsby-plugin-offline',
+	],
+}
+
